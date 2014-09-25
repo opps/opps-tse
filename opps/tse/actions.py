f629e8fe413a3aec0988de34b27169a4f9ccb0e0
Igor P. Leroy <ip.leroy@gmail.com>
2014-09-25 19:04:34 -0300

    Add celery tasks for fill database

--- opps/tse/actions.py
+++ opps/tse/actions.py
@@ -4,8 +4,30 @@ from __future__ import unicode_literals
 import requests
 import zipfile
 import StringIO
-from unidecode import unidecode
 import unicodecsv as csv
+from unidecode import unidecode
+
+from opps.tse.models import PoliticalParty, Candidate
+
+from django.template.defaultfilters import slugify
+
+
+def format_candidates_csv(line):
+    print line
+    try:
+        pp = line[17].replace(' ', '')
+    except:
+        return False
+    pp = PoliticalParty.objects.get(slug=pp)
+    bio = '{0} - Sexo: {1} - Escolaridade: {2}'.format(
+        line[10], line[29], line[31])
+    return {
+        'name': line[13],
+        'number': line[12],
+        'political_party': pp,
+        'slug': slugify(line[13]),
+        'bio': bio,
+    }
 
 
 def parse_candidates_csv(url):
@@ -13,5 +35,38 @@ def parse_candidates_csv(url):
     z = zipfile.ZipFile(StringIO.StringIO(r.content))
     for filename in z.namelist():
         file = z.open(filename)
-        for r in csv.reader(file, delimiter=';', quoting=csv.QUOTE_NONE):
-            print unidecode(r)
+        for line in file.readlines():
+            line = unidecode(line).replace('"', '')
+            line = line.split(';')
+            candidate = format_candidates_csv(line)
+            if not candidate:
+                continue
+            c, created = Candidate.objects.get_or_create(
+                number=candidate['number'])
+            try:
+                if created:
+                    c.name = candidate['name']
+                    c.bio = candidate['bio']
+                    c.political_party = candidate['political_party']
+                    c.slug = candidate['slug']
+                    c.save()
+            except:
+                pass
+
+
+def parse_party_csv(url):
+    r = requests.get(url)
+    content = unidecode(r.content)
+    reader = csv.reader(content.split('\n'))
+    for line in reader:
+        print line
+        if line:
+            lines = line[0].split('\t')
+        try:
+            pp, created = \
+                PoliticalParty.objects.get_or_create(number=int(lines[2]))
+            pp.slug = lines[0]
+            pp.name = lines[1]
+            pp.save()
+        except:
+            pass
--- opps/tse/tasks.py
+++ opps/tse/tasks.py
@@ -1,9 +1,30 @@
 # -*- coding:utf-8 -*-
 from __future__ import unicode_literals
 
-from opps.tse import OPPS_TSE_CANDIDATES_CSV_URL
-from opps.tse.actions import parse_candidates_csv
+from opps.tse.actions import parse_candidates_csv, parse_party_csv
+from opps.tse.models import Election
+from opps.tse import (
+    OPPS_TSE_CANDIDATES_CSV_URL, OPPS_TSE_POLITICAL_PARTY_CSV, slugs)
 
 
 def populate_candidates():
     return parse_candidates_csv(OPPS_TSE_CANDIDATES_CSV_URL)
+
+
+def populate_jobs(year=2014):
+    jobs = ('de', 'df', 'g', 's')
+    print '[Populate jobs and elections]'
+    for j in jobs:
+        for s in slugs:
+            Election.objects.get_or_create(year=year, job=j, state=s)
+    Election.objects.get_or_create(year=year, job='ps')
+
+
+def populate_party():
+    return parse_party_csv(OPPS_TSE_POLITICAL_PARTY_CSV)
+
+
+def populate():
+    populate_party()
+    populate_jobs()
+    populate_candidates()
