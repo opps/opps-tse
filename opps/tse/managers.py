4c583968ec93de1d2294e584f7277bcb005f1992
Lucas Simon <lucassrod@gmail.com>
2014-09-27 00:32:26 -0300

    Create a managers to simplify querysets of models

---
+++ opps/tse/managers.py
@@ -0,0 +1,90 @@
+# -*- coding:utf-8 -*-
+
+# Core Django imports
+from django.db import models
+from django.utils.translation import ugettext as _
+
+# Relative imports of the 'app-name' package
+
+
+class PoliticalPartyQueryset(models.query.QuerySet):
+    u"""
+    Classe para definir os querysets do model
+    de partido politico
+    """
+    pass
+
+
+class PoliticalPartyManager(models.Manager):
+    u"""
+    Define um manager para o model de partido
+    politico
+    """
+    def get_queryset(self):
+        return PoliticalPartyQueryset(self.model, using=self._db)
+
+    # TODO: Fazer manager para filtro de partidos pelo slug
+    # o valor do parametro deve ser em letra maiuscula
+    pass
+
+
+class CandidateQueryset(models.query.QuerySet):
+    u"""
+    Classe para definir os querysets do model
+    de candidato
+    """
+    pass
+
+
+class CandidateManager(models.Manager):
+    u"""
+    Define um manager para o model de candidato
+    """
+    def get_queryset(self):
+        return CandidateQueryset(self.model, using=self._db)
+
+    # TODO: Fazer manager para filtro de partidos politicos
+    pass
+
+
+class ElectionQueryset(models.query.QuerySet):
+    u"""
+    Classe para definir os querysets do model
+    de eleições
+    """
+    pass
+
+
+class ElectionManager(models.Manager):
+    u"""
+    Define um manager para o model de eleições
+    """
+    def get_queryset(self):
+        return ElectionQueryset(self.model, using=self._db)
+
+    # TODO: Fazer manager para filtro de estados
+    # TODO: Fazer manager para cargos da eleicoao
+    pass
+
+
+class VoteQueryset(models.query.QuerySet):
+    u"""
+    Classe para definir os querysets do model
+    de votação
+    """
+    pass
+
+
+class VoteManager(models.Manager):
+    u"""
+    Define um manager para o model de votação
+    """
+    def get_queryset(self):
+        return VoteQueryset(self.model, using=self._db)
+
+    # TODO: Fazer manager para turnos 1 e 2
+    # TODO: Fazer manager para melhor votados
+    # TODO: Fazer manager para limitar o numero de objetos
+    # por exempĺo 5 melhores 3 melhores 10 melhores
+    # atraves da propriedade percent
+    pass
