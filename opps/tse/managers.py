a1377b0d208ff12a8f2a0778c467b3a3e6856cc3
Igor P. Leroy <ip.leroy@gmail.com>
2014-09-30 11:22:37 -0300

    Translate docstring for english

--- opps/tse/managers.py
+++ opps/tse/managers.py
@@ -3,24 +3,17 @@ from __future__ import unicode_literals
 
 from random import randint, sample
 
-# Core Django imports
 from django.db import models
 from django.db.models import Count
 
 from django.utils.translation import ugettext as _
 
-# Relative imports of the 'app-name' package
-
 
 class PoliticalPartyQueryset(models.query.QuerySet):
-    u"""
-    Classe para definir os querysets do model
-    de partido politico
-    """
 
     def party(self, party_name):
-        u"""
-        :param party_name: Parametro do tipo slug
+        """
+        :param party_name: slug format name
         """
         return self.filter(
             slug=party_name.upper()
@@ -28,33 +21,22 @@ class PoliticalPartyQueryset(models.query.QuerySet):
 
 
 class PoliticalPartyManager(models.Manager):
-    u"""
-    Define um manager para o model de partido
-    politico
-    """
     def get_queryset(self):
         return PoliticalPartyQueryset(self.model, using=self._db)
 
     def party(self, party):
-        u"""
-        Resgata os resultados pelo slug da classe model
-        :param party_name: Parametro do tipo slug
+        """
+        :param party_name: slug format name
         """
         return self.get_queryset().party(party)
 
 
 class CandidateQueryset(models.query.QuerySet):
-    u"""
-    Classe para definir os querysets do model
-    de candidato
-    """
     def party(self, party_name):
-        u"""
-        :param party_name: Parametro do tipo slug
         """
-        return self.filter(
-            political_party__slug=party_name.upper()
-        )
+        :param party_name: slug format name
+        """
+        return self.filter(political_party__slug=party_name.upper())
 
     def main(self):
         return self.filter(is_main=True)
@@ -78,172 +60,132 @@ class CandidateQueryset(models.query.QuerySet):
 
 
 class CandidateManager(models.Manager):
-    u"""
-    Define um manager para o model de candidato
-    """
     def get_queryset(self):
         return CandidateQueryset(self.model, using=self._db)
 
     def by_party(self, party):
-        u"""
-        Resgata os resultados pelo slug da classe model
-        :param party_name: Parametro do tipo slug
+        """
+        :param party_name: slug format name
         """
         return self.get_queryset().party(party)
 
     def featured(self):
-        """
-        """
         return self.get_queryset().featured()
 
     def random(self):
-        """
-        """
         return self.get_queryset().random()
 
     def randoms(self, qty):
-        """
-        """
         return self.get_queryset().randoms(qty)
 
 
 class ElectionQueryset(models.query.QuerySet):
-    u"""
-    Classe para definir os querysets do model
-    de eleições
-    """
 
     def year(self, y):
-        u"""
-        :param y: Ano da eleição como inteiro positivo
+        """
+        :param y: integer field
         """
         return self.filter(year=y)
 
     def state(self, state):
-        u"""
-        :param state: Parametro do com uma string contendo
-        a sigla em maiuscula do estado
+        """
+        :param state: state slug
         """
         return self.filter(state=state.upper())
 
     def job(self, j):
-        u"""
-        :param j: Parametro do tipo string referenciado pela
-        tupla de jobs do models.py
+        """
+        :param j: string job at opps.tse.models.JOBS
         """
         return self.filter(job=j)
 
     def jobs(self, j):
-        u"""
-        :param j: Lista contendo strings dos jobs
-        tupla de jobs do models.py
+        """
+        :param j: string job at opps.tse.models.JOBS
         """
         return self.filter(job__in=j)
 
 
 class ElectionManager(models.Manager):
-    u"""
-    Define um manager para o model de eleições
-    """
     def get_queryset(self):
         return ElectionQueryset(self.model, using=self._db)
 
     def year(self, y):
-        u"""
-        Retorna lista com as eleicoes do ano
-        correspondente pelo parametro
-        :param y: Ano da eleição como inteiro positivo
+        """
+        :param y: integer field
         """
         return self.get_queryset().year(y)
 
     def state(self, state):
-        u"""
-        Retorna lista com as eleicoes daquele estado
-        :param state: Parametro do com uma string contendo
-        a sigla em maiuscula do estado
+        """
+        :param state: state slug
         """
         return self.get_queryset().state(state)
 
     def job(self, j):
-        u"""
-        Retorna lista das eleicoes especificando um
-        cargo
-        :param j: Parametro do tipo string referenciado pela
-        tupla de jobs do models.py
+        """
+        :param j: string job at opps.tse.models.JOBS
         """
         return self.get_queryset().job(j)
 
     def jobs(self, j):
-        u"""
-        Retorna lista das eleicoes especificando um
-        lista de cargos
-        :param j: Lista contendo strings dos jobs
-        tupla de jobs do models.py
+        """
+        :param j: string job at opps.tse.models.JOBS
         """
         return self.get_queryset().jobs(j)
 
 
 class VoteQueryset(models.query.QuerySet):
-    u"""
-    Classe para definir os querysets do model
-    de votação
-    """
-
     def party(self, party_name):
-        u"""
-        :param party_name: Parametro do tipo slug
+        """
+        :param party_name: slug format name
         """
         return self.filter(
             candidate__political_party__slug=party_name.upper()
         )
 
     def year(self, y):
-        u"""
-        :param y: Ano da eleição como inteiro positivo
+        """
+        :param y: integer field
         """
         return self.filter(election__year=y)
 
     def state(self, state):
-        u"""
-        :param state: Parametro do com uma string contendo
-        a sigla em maiuscula do estado
+        """
+        :param state: state slug
         """
         return self.filter(election__state=state.upper())
 
     def job(self, j):
-        u"""
-        :param j: Parametro do tipo string referenciado pela
-        tupla de jobs do models.py
+        """
+        :param j: string job at opps.tse.models.JOBS
         """
         return self.filter(election__job=j)
 
     def jobs(self, j):
-        u"""
-        :param j: Lista contendo strings dos jobs
-        tupla de jobs do models.py
+        """
+        :param j: string job at opps.tse.models.JOBS
         """
         return self.filter(election__job__in=j)
 
     def latest_with_limits(self, l):
         """
-        :param l: Número para limitar a busca
+        :param l: integer field
         """
         return self.order_by("-votes")[:l]
 
     def elected(self):
-        u"""
-        """
         return self.filter(is_elected=True)
 
     def turn(self, t):
         """
-        :param l: Número para limitar a busca
+        :param l: integer field
         """
         if t not in [1, 2]:
             raise ValueError(
                 _(
-                    u'Parameter was invalid!' +
-                    u'Accepts 1 or 2 values.'
+                    'Parameter was invalid!' +
+                    'Accepts 1 or 2 values.'
                 )
             )
 
@@ -262,45 +204,44 @@ class VoteManager(models.Manager):
 
     def latest_with_limits(self, limit):
         """
-        Retorna os ultimas votacao
-        de acordo com o limit setado
-        :param limit: Integer seta o limite da busca
+        :param limit: integer field
         """
         return self.get_queryset().latest_with_limits(limit)
 
     def party(self, party_name):
-        u"""
-        Retorna as votacoes pelo partidos politicos do candidato
+        """
+        :param l: slug format name
         """
         return self.get_queryset().party(party_name)
 
     def year(self, y):
-        u"""
-        Retorna as votacoes pelo ano da eleicao
+        """
+        :param y: integer field
         """
         return self.get_queryset().year(y)
 
     def state(self, state):
-        u"""
-        Retorna as votações pelo estado
+        """
+        :param state: state slug
         """
         return self.get_queryset().state(state)
 
     def job(self, j):
-        u"""
-        Retorna as votações pelo cargo
+        """
+        :param j: string job at opps.tse.models.JOBS
         """
         return self.get_queryset().job(j)
 
     def jobs(self, j):
-        u"""
-        Retorna as votações pelos cargos
+        """
+        :param j: string job at opps.tse.models.JOBS
         """
         return self.get_queryset().jobs(j)
 
     def turn(self, t):
         """
-        Retorna as votações por turno
+        Get election by turn
+        :param t: integer field (1, 2)
         """
         if [1, 2] not in t:
             raise ValueError(
@@ -313,21 +254,16 @@ class VoteManager(models.Manager):
         return self.filter(turn=t)
 
     def elected(self):
-        u"""
-        Retorna as votações de quem foi eleito
-        """
         return self.get_queryset().elected()
 
     def best_votes(self):
         """
-        Retorna os melhores votados.
-        Este manager é melhor utilizado combando
-        com outros manager como turno, state, job
+        Return the most voted candidates
         """
         return self.get_queryset().best_votes()
 
-    def featured(self):
+    def main(self):
         """
-        Retorna os candidatos da votação em destaque
+        Return the main candidates
         """
-        return self.get_queryset().featured()
+        return self.get_queryset().main()
